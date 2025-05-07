7be1a776d067553ce6b6f5ca01ebe0bcd6592bbe
John Zhou <john-xyz@outlook.com>
2025-05-06 21:32:30 -0500

    more throughly test modulemap

--- PyDE/PyDE-Bridging-Header.h
+++ PyDE/PyDE-Bridging-Header.h
@@ -2,6 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <Python/Python.h>
+@import Python;
 PyObject* call_method_noargs(PyObject* obj, const char* method);
 void InitInterp(void);
